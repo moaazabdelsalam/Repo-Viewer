91b77e0faa9ff48c88c24f90d5720bec8fb7ed26
Moaaz AbdEl-Salam <moaazabdelsalam99@gmail.com>
2024-01-02 14:01:54 +0200

    added search bar to HomeScreen

--- app/src/main/java/com/task/githuprepoviewer/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/task/githuprepoviewer/presentation/home/HomeScreen.kt
@@ -1,7 +1,7 @@
 package com.task.githuprepoviewer.presentation.home
 
+import android.util.Log
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,14 +11,22 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -74,12 +82,13 @@ fun HomeReposList(
             loadMore()
         }
     }
-    Box(
-        contentAlignment = Alignment.Center,
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
             .padding(10.dp)
     ) {
+        MySearchBar({Log.i("TAG HomeScreen", "search: $it")})
         LazyColumn(state = listState) {
             items(
                 list,
@@ -93,6 +102,47 @@ fun HomeReposList(
                 )
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MySearchBar(
+    onQueryChange: (String) -> Unit
+) {
+    var searchText by remember { mutableStateOf("") }
+    SearchBar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        query = searchText,
+        onQueryChange = {
+            searchText = it
+            onQueryChange(searchText)
+        },
+        onSearch = {},
+        active = false,
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = "Search Icon"
+            )
+        },
+        trailingIcon = {
+            if (searchText.isNotBlank()) {
+                Icon(
+                    modifier = Modifier.clickable {
+                        searchText = ""
+                    },
+                    imageVector = Icons.Outlined.Close,
+                    contentDescription = "Clear Icon"
+                )
+            }
+        },
+        placeholder = { Text(text = "Search with Repo Name or Owner") },
+        onActiveChange = {}
+    ) {
+
     }
 }
 
